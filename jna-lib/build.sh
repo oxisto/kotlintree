6689460e781af11ee62852c4cdf39facaeef8907
Christian Banse <oxisto@aybaze.com>
2021-12-10 11:48:32 +0100

    Building into resources folder

--- jna-lib/build.sh
+++ jna-lib/build.sh
@@ -3,14 +3,14 @@
 ARCH=`uname -s`
 TREE_SITTER_VERSION=0.20.1
 TREE_SITTER_CPP_VERSION=0.19.0
-#wget https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v$TREE_SITTER_VERSION.tar.gz -O tree-sitter-$TREE_SITTER_VERSION.tar.gz
-#wget https://github.com/tree-sitter/tree-sitter-cpp/archive/refs/tags/v$TREE_SITTER_CPP_VERSION.tar.gz -O tree-sitter-cpp-$TREE_SITTER_CPP_VERSION.tar.gz
+wget https://github.com/tree-sitter/tree-sitter/archive/refs/tags/v$TREE_SITTER_VERSION.tar.gz -O tree-sitter-$TREE_SITTER_VERSION.tar.gz
+wget https://github.com/tree-sitter/tree-sitter-cpp/archive/refs/tags/v$TREE_SITTER_CPP_VERSION.tar.gz -O tree-sitter-cpp-$TREE_SITTER_CPP_VERSION.tar.gz
 
-#tar -xzvf tree-sitter-$TREE_SITTER_VERSION.tar.gz
-#tar -xzvf tree-sitter-cpp-$TREE_SITTER_CPP_VERSION.tar.gz
+tar -xzvf tree-sitter-$TREE_SITTER_VERSION.tar.gz
+tar -xzvf tree-sitter-cpp-$TREE_SITTER_CPP_VERSION.tar.gz
 
-export LIBDIR=`pwd`
-export INSTALL_PATH=`pwd`
+export LIBDIR=`pwd`/src/main/resources
+export INSTALL_PATH=`pwd`/src/main/resources
 
 cd tree-sitter-$TREE_SITTER_VERSION
 
@@ -41,12 +41,12 @@ if [ $ARCH == "Linux" ]; then
           make clean
                   make
                   make install
+        cd ../tree-sitter-cpp-0.19.0/src
           export SONAME_MAJOR=0
           export SONAME_MINOR=0
           export SOEXT=so
   	    export SOEXTVER_MAJOR=so.$SONAME_MAJOR
   	    export SOEXTVER=so.$SONAME_MAJOR.$SONAME_MINOR
-          export LIBDIR=$PREFIX/lib
           clang++ $CFLAGS $LDFLAGS -I. scanner.cc parser.c -shared -Wl,-soname,$LIBDIR/libtree-sitter-cpp.so.$SONAME_MAJOR -o libtree-sitter-cpp.$SOEXTVER
           cp libtree-sitter-cpp.$SOEXTVER $LIBDIR
           cd $LIBDIR
