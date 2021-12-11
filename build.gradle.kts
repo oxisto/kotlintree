c357e5d07d2d6cf0c223820ab25d640116d176d8
Christian Banse <oxisto@aybaze.com>
2021-12-11 15:39:38 +0100

    Suppressing inherited JNA members

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-    id("org.jetbrains.dokka") version "1.6.0" apply false
+    id("org.jetbrains.dokka") version "1.6.0"
     id("com.diffplug.spotless") version "6.0.0"
 
     kotlin("jvm") version "1.6.0" apply false
 }
 
 subprojects {
+    apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "com.diffplug.spotless")
 
     tasks.withType<KotlinCompile> {
         dependsOn("spotlessApply")
+    }
+
+    tasks.dokkaHtml.configure {
+        // intentionally hide inherited members, otherwise we will have a lot of functions inherited from JNA
+        suppressInheritedMembers.set(true)
     }
 
     spotless {
@@ -26,8 +33,11 @@ repositories {
 }
 
 allprojects {
-    apply(plugin = "org.jetbrains.dokka")
-
     group = "io.github.oxisto"
     version = "0.0.0-SNAPSHOT"
+}
+
+tasks.dokkaHtmlCollector.configure{
+    // intentionally hide inherited members, otherwise we will have a lot of functions inherited from JNA
+    suppressInheritedMembers.set(true)
 }
